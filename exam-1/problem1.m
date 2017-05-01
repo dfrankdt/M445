b4e5b06d1e1fb24b722d873b3fe0c8767b920815
Frank Lynch <fhlynch@gmail.com>
2017-05-01 12:09:59 -0700

    Files for Exam 1

---
+++ exam-1/problem1.m
@@ -0,0 +1,19 @@
+% problem1.m
+clear
+
+% Thermal conductivity C is given as a function of temperature T.
+
+T = [ 100
+      200
+      300
+      400
+      500
+      600];
+
+C =[  9.4
+      18.4
+      26.2
+      33.3
+      39.7
+      45.7];
+      
